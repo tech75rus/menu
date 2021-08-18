4c9d01136d1558552dd8a58fbaa6e570d0ada7a7
tech75rus <tech75rus@mail.ru>
2021-08-18 20:42:03 +0900

    сделал меню и контент на div и float. Внутри контента grid

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/flex',
+    name: 'Flex',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Flex')
+  },
+  {
+    path: '/float',
+    name: 'Float',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Float')
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/Float.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="window">
+		<div class="main">
+			<div id="nav-clear">nav-clear</div>
+			<div class="content">
+				<div>asdcontent</div>
+				<div>content</div>
+				<div>content</div>
+				<div>content</div>
+			</div>
+		</div>
+		<div id="nav">
+			nav
+			<button @click="changeMenu">click</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Float',
+	methods: {
+		changeMenu() {
+			let nav = document.getElementById('nav');
+			let navClear = document.getElementById('nav-clear');
+
+			if (nav.style.width === '70px') {
+				nav.style.width = '110px';
+				navClear.style.width = '130px';
+				return;
+			}
+			nav.style.width = '70px';
+			navClear.style.width = '90px';
+
+		}
+	}
+}
+</script>
+<style lang="scss" scoped>
+@import "src/assets/scss/menu";
+</style>
